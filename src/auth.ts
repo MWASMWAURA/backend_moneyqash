e94e091da0e0e9916ebc72e8cf85713a2eab6ade
MWASMWAURA <jamesmwauramwas@gmail.com>
2025-07-27 10:34:10 +0300

    added new cookie settings

--- src/auth.ts
+++ src/auth.ts
@@ -29,6 +29,8 @@ export function setupAuth(app: Express): void {
     store: storage.sessionStore,
     cookie: {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      sameSite: 'none',
+      secure: true
     }
   };
 
